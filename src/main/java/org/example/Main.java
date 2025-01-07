41b53851fbf2a0ededeb377792ead1aedf90dac3
melisa-kabasakal <meliskabasakal33@gmail.com>
2025-01-07 16:37:35 +0300

    task 1 and 2 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,61 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+
+        System.out.println(numberToWords(123));
+        System.out.println(numberToWords(1010));
+        System.out.println(numberToWords(-12));
+    }
+
+    public static boolean isPalindrome(int number) {
+        int numbersOrgin = Math.abs(number);
+        int backWards = 0;
+        int numbersTwo = numbersOrgin;
+
+        while (numbersTwo > 0) {
+            int lastNumbers = (numbersTwo % 10);
+            backWards = (backWards * 10) + lastNumbers;
+            numbersTwo /= 10;
+        }
+
+        numbersTwo = backWards;
+
+        while (numbersTwo > 0) {
+            int lastNumbers = numbersTwo % 10;
+            numbersTwo /= 10;
+        }
+        return numbersOrgin == backWards;
+    }
+
+    public static  boolean isPerfectNumber(int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+
+            }
+        }
+        return  sum == number;
+    }
+
+    public static String numberToWords(int number){
+        if (number < 0) {
+            return "Invalid Value";
+        }
+        
+
     }
 }
